d8afbc9236cab543387b379169da3a1615dfac85
Сергей Азаренко <s_azarenko@outlook.com>
2020-02-20 12:14:55 +0300

    Добавление команд

--- BinomialHeap/BinomialHeap/BinomialHeap.cs
+++ BinomialHeap/BinomialHeap/BinomialHeap.cs
@@ -9,7 +9,33 @@ namespace BinomialHeap.BinomialHeap
     class BinomialHeap
     {
         //Поля
-        List<char> Binary; // Для быстродействия можно отслеживать степени деревьяев, а не массив (есть стпепень - 1, нет - 0)
+        List<int> Binary; // Для быстродействия можно отслеживать степени деревьяев, а не массив (есть стпепень - 1, нет - 0)
         Heap head;
+
+        public void Insert(Heap heap)
+        {
+            // Вот здесь используем {0,1,2}-систему
+        }
+
+        private void Merge(Heap h1, Heap h2)
+        {
+
+        }
+
+        public Heap[] FindHeap(int degree)
+        {
+            // Найти пирамиду по степени
+            return null;
+        }
+
+        public void Delete(Heap hp)
+        {
+
+        }
+
+        public int GetMin()
+        {
+            return 0;
+        }
     }
 }
